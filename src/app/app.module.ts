d9aab0c900f65d13206a80ec9203afb151cfd249
Pham Van Truong <pvtruong115@gmail.com>
2018-06-22 11:01:48 +0700

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
+// Other imports removed for brevity
+
+import { NgxsModule } from '@ngxs/store';
+import { TutorialState } from './state/tutorial.state';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadComponent,
+    CreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxsModule.forRoot([
+      TutorialState
+    ]),
+    NgxsReduxDevtoolsPluginModule,
+    NgxsLoggerPluginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
